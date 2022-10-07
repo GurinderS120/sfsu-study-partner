39eeb30b0ec8aa54c94f76c256ccbf198d0575e2
GurinderS120 <gsingh14@mail.sfsu.edu>
2022-10-07 07:49:14 -0700

    added comments to explain the purpose of _app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,13 @@ import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
+    // Component represents a page and ...pageProps represents the data (if any)
+    // we want to pass as props to that page.
+    // Layout is a component that we created in component/layout file and it
+    // contains basic styles and components that we want to display on every page
+    // like navbar, footer, etc.
+    // So we wrap each page (represented by Component) in the Layout component
+    // and in the Layout component we display the page (Component) as its child.
     <Layout>
       <Component {...pageProps} />
     </Layout>
