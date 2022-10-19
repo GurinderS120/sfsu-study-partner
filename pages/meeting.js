b0beb938339a11d29abe38582ce1dfd57da4fe2a
GurinderS120 <gsingh14@mail.sfsu.edu>
2022-10-19 16:04:15 -0700

    Docs(meeting): Add descriptive comment to explain 'ssr' behavior

--- pages/meeting.js
+++ pages/meeting.js
@@ -1,6 +1,11 @@
 import dynamic from "next/dynamic";
 import { useState } from "react";
 
+// Since zoom functions use "window" object along with other DOM objects
+// so we cannot render the "ZoomMeeting" component on the server, since
+// there is no "window" object available in Node runtime environment.
+// Because of which we specify "ssr: false" (set server-side-rendering to
+// false)
 const ZoomMeeting = dynamic(() => import("../components/zoomMeeting"), {
   ssr: false,
 });
