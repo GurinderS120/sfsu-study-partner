759d17de91aa622c42b74d675368666868edc0c9
GurinderS120 <gsingh14@mail.sfsu.edu>
2022-10-24 17:11:10 -0700

    Comment(_app.js): Uncomment the globals css import

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "../components/layout";
-// import "../styles/globals.css";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
