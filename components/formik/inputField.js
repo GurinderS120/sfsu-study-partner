aca6f0e6349e6d4dc211b350cf5cb2bc8dc8280e
GurinderS120 <gsingh14@mail.sfsu.edu>
2022-10-24 17:13:02 -0700

    Add(inputField): Add InputField component

---
+++ components/formik/inputField.js
@@ -0,0 +1,22 @@
+import { useField } from "formik";
+
+const InputField = (props) => {
+  const [field, meta] = useField(props);
+  const { name, type, placeholder } = props;
+
+  return (
+    <>
+      <input
+        className={`${
+          meta.touched ? (meta.error ? "is-invalid " : "is-valid ") : ""
+        }form-control mt-3`}
+        {...field}
+        name={name}
+        type={type}
+        placeholder={placeholder}
+      />
+    </>
+  );
+};
+
+export default InputField;
