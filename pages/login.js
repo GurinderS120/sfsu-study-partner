05d053e31965a37658d7ca5c0cba857dcf3621a9
GurinderS120 <gsingh14@mail.sfsu.edu>
2022-10-26 17:41:37 -0700

    Add(login): Add reset password functionality

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,14 @@
 import { Formik, ErrorMessage, Form } from "formik";
 import Button from "react-bootstrap/Button";
-import { getAuth, signOut, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  signOut,
+  signInWithEmailAndPassword,
+  sendPasswordResetEmail,
+} from "firebase/auth";
 import { signinSchema } from "../validation/schemas";
+import { useState } from "react";
+import CustomAlert from "../components/alert";
 import InputField from "../components/formik/inputField";
 import Link from "next/link";
 
@@ -30,42 +37,102 @@ async function login(values) {
   }
 }
 
+// Async function that is responsible for sending a password resetting email to
+// users by connecting to Firebase
+async function resetPassword(
+  email,
+  erorrs,
+  setIsAlert,
+  setVariant,
+  setMessage
+) {
+  const app = (await import("../firebase/config")).app;
+  const auth = getAuth(app);
+
+  try {
+    if (email === "" || erorrs) {
+      setIsAlert(true);
+      setVariant("danger");
+      setMessage("Please provide valid SFSU email");
+      return;
+    }
+
+    await sendPasswordResetEmail(auth, email);
+
+    setIsAlert(true);
+    setVariant("info");
+    setMessage("Please check your SFSU email for resetting your password");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 function Login() {
+  const [isAlert, setIsAlert] = useState(false);
+  const [variant, setVariant] = useState("info");
+  const [message, setMessage] = useState("");
+
   return (
-    <Formik
-      initialValues={{ email: "", password: "" }}
-      onSubmit={login}
-      validationSchema={signinSchema}
-    >
-      <div className="form-container">
-        <Form className="d-flex flex-column">
-          <h3 className="form-header">Login</h3>
+    <>
+      {isAlert && <CustomAlert variant={variant} message={message} />}
 
-          <InputField type="email" name="email" placeholder="Email" />
-          <ErrorMessage
-            component="p"
-            name="email"
-            className="text-danger small mb-0"
-          />
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={login}
+        validationSchema={signinSchema}
+      >
+        {(props) => (
+          <div className="form-container">
+            <Form className="d-flex flex-column">
+              <h3 className="form-header">Login</h3>
 
-          <InputField type="password" name="password" placeholder="Password" />
-          <ErrorMessage
-            component="p"
-            name="password"
-            className="text-danger small mb-0"
-          />
+              <InputField type="email" name="email" placeholder="Email" />
+              <ErrorMessage
+                component="p"
+                name="email"
+                className="text-danger small mb-0"
+              />
 
-          <Button variant="primary" className="mt-3" type="submit">
-            Login
-          </Button>
+              <InputField
+                type="password"
+                name="password"
+                placeholder="Password"
+              />
+              <ErrorMessage
+                component="p"
+                name="password"
+                className="text-danger small mb-0"
+              />
 
-          <h6 className="mt-3 text-center">
-            <span className="me-1">Don&apos;t have an account?</span>
-            <Link href="/register">Register</Link>
-          </h6>
-        </Form>
-      </div>
-    </Formik>
+              <p
+                onClick={() =>
+                  resetPassword(
+                    props.values.email,
+                    props.errors.email,
+                    setIsAlert,
+                    setVariant,
+                    setMessage
+                  )
+                }
+                role="button"
+                className="align-self-end small mt-1 mb-0 text-muted cursor"
+              >
+                Forgot/Reset password?
+              </p>
+
+              <Button variant="primary" className="mt-4" type="submit">
+                Login
+              </Button>
+
+              <h6 className="mt-3 text-center">
+                <span className="me-1">Don&apos;t have an account?</span>
+                <Link href="/register">Register</Link>
+              </h6>
+            </Form>
+          </div>
+        )}
+      </Formik>
+    </>
   );
 }
 
