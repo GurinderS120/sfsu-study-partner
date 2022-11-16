08e6845856591b30334cf20a48a51f669e92e02b
GurinderS120 <gsingh14@mail.sfsu.edu>
2022-11-16 06:16:37 -0800

    Add(studyRoom): Add some testing code

--- pages/app/studyRoom.js
+++ pages/app/studyRoom.js
@@ -1,5 +1,32 @@
+import { useEffect, useState } from "react";
+import { selectUser } from "../../reduxStateManagement/slices/userSlice";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+
 function StudyRoom() {
-  return <div>Create a study room</div>;
+  const user = useSelector(selectUser);
+
+  // useEffect(() => {
+  //   async function getRoom() {
+  //     console.log(user);
+  //     //If the management token is expired or doesn't exist, fetch it
+  //     const response = await fetch("/api/managementToken");
+  //     const { token } = await response.json();
+
+  //     //Once we have the management token we can use it to retrieve
+  //     //user's room
+
+  //     // console.log(`The roomId for ${user.name} is: ${id}`);
+
+  //     //Once we have roomId we can join the room as host
+  //   }
+
+  //   console.log("In useEffect");
+  //   if (user) {
+  //     getRoom();
+  //   }
+  // }, [user?.name]);
+  return <>{user?.roomId}</>;
 }
 
 export default StudyRoom;
