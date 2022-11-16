37945fbb7d62086a032b123a9e0dc03a79882dae
GurinderS120 <gsingh14@mail.sfsu.edu>
2022-11-16 06:09:35 -0800

    Add(uploadProfile): Add createRoom functionality

--- helperFunctions/uploadProfile.js
+++ helperFunctions/uploadProfile.js
@@ -7,16 +7,45 @@ async function uploadProfileToDatabase(profInfo) {
   const db = getFirestore(app);
 
   try {
+    const roomId = await createRoom(values.name);
+
     await setDoc(doc(db, "users", user.uid), {
       name: values.name,
       major: values.major,
       pic: {
         url: imgStrgRef,
       },
+      roomId: roomId,
     });
+
+    return roomId;
   } catch (error) {
     alert(error.message);
   }
+}
+
+async function createRoom(name) {
+  //If the management token is expired or doesn't exist, fetch it
+  const response = await fetch("/api/managementToken");
+  const { token } = await response.json();
+
+  //Once we have the management token we can use it to create/retrieve
+  //user's room
+  const roomResponse = await fetch("https://api.100ms.live/v2/rooms", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      name: `${name.replaceAll(" ", "-")}-room`,
+      template_id: "6373e948e150dc0de7ce33dc",
+      region: "us",
+    }),
+  });
+
+  const { id } = await roomResponse.json();
+  return id;
 }
 
 export default uploadProfileToDatabase;
