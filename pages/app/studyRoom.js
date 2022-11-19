c977401c174426f7ec89b0a50e6d50904cbe671e
GurinderS120 <gsingh14@mail.sfsu.edu>
2022-11-19 09:05:16 -0800

    Add(studyRoom): Add various components to the study room

--- pages/app/studyRoom.js
+++ pages/app/studyRoom.js
@@ -1,32 +1,85 @@
 import { useEffect, useState } from "react";
 import { selectUser } from "../../reduxStateManagement/slices/userSlice";
 import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
+import Conference from "../../components/videoConferencing/conference";
+import Container from "react-bootstrap/Container";
+import CustomAlert from "../../components/alert";
+import RoomStatus from "../../components/videoConferencing/roomStatus";
+
+// import {
+//   selectIsConnectedToRoom,
+//   useHMSActions,
+//   useHMSStore,
+// } from "@100mslive/react-sdk";
+import Spinner from "react-bootstrap/Spinner";
+
+async function joinRoom(name, token, hmsActions) {
+  //Join the room
+  await hmsActions.join({
+    userName: name,
+    authToken: token,
+  });
+}
 
 function StudyRoom() {
   const user = useSelector(selectUser);
+  // const isConnected = useHMSStore(selectIsConnectedToRoom);
+  const isConnected = true;
+  const [isAlert, setIsAlert] = useState(false);
+  const [variant, setVariant] = useState("warning");
+  const [message, setMessage] = useState("message");
+  // const hmsActions = useHMSActions();
 
   // useEffect(() => {
-  //   async function getRoom() {
-  //     console.log(user);
-  //     //If the management token is expired or doesn't exist, fetch it
-  //     const response = await fetch("/api/managementToken");
-  //     const { token } = await response.json();
+  //   // async function getUserToken() {
+  //   //   //Retrieve user token (used in joining a room)
+  //   //   const roomResponse = await fetch("/api/userToken", {
+  //   //     method: "POST",
+  //   //     headers: {
+  //   //       "Content-Type": "application/json",
+  //   //     },
+  //   //     body: JSON.stringify({
+  //   //       roomId: user.roomId,
+  //   //       userId: user.uid,
+  //   //     }),
+  //   //   });
+  //   //   const { token } = await roomResponse.json();
+  //   //   joinRoom(user.name, token, hmsActions);
+  //   // }
+  //   // if (user) {
+  //   //   getUserToken();
+  //   // }
+  // }, [user]);
 
-  //     //Once we have the management token we can use it to retrieve
-  //     //user's room
+  // useEffect(() => {
+  //   window.onunload = () => {
+  //     if (isConnected) {
+  //       hmsActions.leave();
+  //     }
+  //   };
+  // }, [hmsActions, isConnected]);
 
-  //     // console.log(`The roomId for ${user.name} is: ${id}`);
-
-  //     //Once we have roomId we can join the room as host
-  //   }
-
-  //   console.log("In useEffect");
-  //   if (user) {
-  //     getRoom();
-  //   }
-  // }, [user?.name]);
-  return <>{user?.roomId}</>;
+  return isConnected ? (
+    <>
+      {isAlert && (
+        <CustomAlert
+          variant={variant}
+          message={message}
+          setIsAlert={setIsAlert}
+        />
+      )}
+      <Container fluid="md" className="section">
+        <Conference setIsAlert={setIsAlert} setMessage={setMessage} />
+        <RoomStatus />
+      </Container>
+    </>
+  ) : (
+    <div className="vh-100 d-flex justify-content-center align-items-center">
+      <Spinner animation="border" role="status" size="lg">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    </div>
+  );
 }
 
 export default StudyRoom;
