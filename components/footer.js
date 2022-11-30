42482c059be90dab878fc8294ad0e07f11854257
GurinderS120 <gsingh14@mail.sfsu.edu>
2022-11-30 15:45:13 -0800

    Style(footer): Update some footer styles

--- components/footer.js
+++ components/footer.js
@@ -1,43 +1,78 @@
 import React from "react";
 import footerBarStyle from "./footerStyles.module.css";
 import Nav from "react-bootstrap/Nav";
-
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function footerBar() {
-    return (
-        <div className={footerBarStyle['footer-container']}>
-        <div className={footerBarStyle['footer-links']}>
-          <div className={footerBarStyle['footer-link-wrapper']}>
-            <div className={footerBarStyle['footer-link-items']}>
-              <h2>Menu</h2>
-                <Nav.Link className = {footerBarStyle["text"]} href='/'>Home</Nav.Link>
-                <Nav.Link className = "text" href='/'>About</Nav.Link>
-                <Nav.Link className = "text" href='/'>Product</Nav.Link>
-            </div>
-          </div>
-            <div className={footerBarStyle['footer-link-items']}>
-              <h2>Services</h2>
-              <Nav.Link className = "text" href='/'>Study Rooms</Nav.Link>
-              <Nav.Link className = "text" href='/'>Scehdule Meeting</Nav.Link>
-              <Nav.Link className = "text" href='/'>Invite Others</Nav.Link>
-            </div>
-            <div className={footerBarStyle['footer-link-items']}>
-              <h2>Social Media</h2>
-              <Nav.Link className = "text" href='/'>Facebook</Nav.Link>
-              <Nav.Link className = "text" href='/'>Twitter</Nav.Link>
-              <Nav.Link className = "text" href='/'>Instagram</Nav.Link>
-            </div>
-            <div className={footerBarStyle['footer-link-items']}>
-              <h2>Get In Touch</h2>
-              <Nav.Link className = "text" href='/'>Email</Nav.Link>
-              <Nav.Link className = "text" href='/'>415-000-000</Nav.Link>
-              <Nav.Link className = "text" href='/'>LinkedIn</Nav.Link>
-            </div>
-          </div>
-          <small className={footerBarStyle['website-rights']}>SFSU Study Partners est. 2022 © SFSU</small>
-         </div>
-    );
+  return (
+    <Container className="bg-dark py-4" fluid>
+      {/*  className={footerBarStyle["footer-links"]} */}
+      <Row className="ms-2">
+        {/* className={footerBarStyle["footer-link-wrapper"]} */}
+        <Col className="text-light">
+          {/* className={footerBarStyle["footer-link-items"]} */}
+          <h2>Menu</h2>
+          <Nav.Link className={footerBarStyle["text"]} href="/">
+            Home
+          </Nav.Link>
+          <Nav.Link className={footerBarStyle["text"]} href="/">
+            About
+          </Nav.Link>
+          <Nav.Link className={footerBarStyle["text"]} href="/">
+            Product
+          </Nav.Link>
+        </Col>
+
+        <Col className="text-light mb-2 mb-md-0">
+          {/* className={footerBarStyle["footer-link-items"]} */}
+          <h2>Services</h2>
+          <Nav.Link className={footerBarStyle["text"]} href="/">
+            Study Rooms
+          </Nav.Link>
+          <Nav.Link className={footerBarStyle["text"]} href="/">
+            Scehdule Meeting
+          </Nav.Link>
+          <Nav.Link className={footerBarStyle["text"]} href="/">
+            Invite Others
+          </Nav.Link>
+          <Nav.Link className="text" href="/">
+            Create a Profile
+          </Nav.Link>
+        </Col>
+        <Col className="text-light">
+          {/* className={footerBarStyle["footer-link-items"]} */}
+          <h2>Social Media</h2>
+          <Nav.Link className={footerBarStyle["text"]} href="/">
+            Facebook
+          </Nav.Link>
+          <Nav.Link className={footerBarStyle["text"]} href="/">
+            Twitter
+          </Nav.Link>
+          <Nav.Link className={footerBarStyle["text"]} href="/">
+            Instagram
+          </Nav.Link>
+        </Col>
+        <Col className="text-light">
+          {/* className={footerBarStyle["footer-link-items"]} */}
+          <h2>Get In Touch</h2>
+          <Nav.Link className={footerBarStyle["text"]} href="/">
+            Email
+          </Nav.Link>
+          <Nav.Link className={footerBarStyle["text"]} href="/">
+            415-000-000
+          </Nav.Link>
+          <Nav.Link className={footerBarStyle["text"]} href="/">
+            LinkedIn
+          </Nav.Link>
+        </Col>
+      </Row>
+      <p className={`${footerBarStyle["website-rights"]} text-md-center`}>
+        SFSU Study Partners est. 2022 © SFSU
+      </p>
+    </Container>
+  );
 }
 
 export default footerBar;
-
